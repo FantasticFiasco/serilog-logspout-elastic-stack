3f21bfe042811d3e574c5c4219692e5fed78a141
FantasticFiasco <mattias.kindborg@gmail.com>
2019-06-18 07:42:02 +0200

    chore: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# serilog-logspout-elastic-stack
-Sample application of Serilog sending log events to Elastic Stack using Logspout.
+# Serilog, Logspout and the Elastic Stack
+
+TODO
---
+++ serilog/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Bogus;
+using Serilog;
+using Serilog.Sinks.Http.BatchFormatters;
+using SerilogExample.Generators;
+
+namespace SerilogExample
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // TODO: Refactor 
+            // ILogger logger = new LoggerConfiguration()
+            //     .WriteTo.DurableHttpUsingFileSizeRolledBuffers(
+            //         requestUri: "http://logstash:31311",
+            //         batchFormatter: new ArrayBatchFormatter())
+            //     .WriteTo.Console()
+            //     .CreateLogger()
+            //     .ForContext<Program>();
+
+            var customerGenerator = new CustomerGenerator();
+            var orderGenerator = new OrderGenerator();
+
+            while (true)
+            {
+                var customer = customerGenerator.Generate();
+                var order = orderGenerator.Generate();
+                
+                logger.Information("{@customer} placed {@order}", customer, order);
+                
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
